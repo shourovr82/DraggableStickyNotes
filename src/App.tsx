5d97b57fdc1d70c657c1e183898435ae2668560f
shourovr82 <shafinur512@gmail.com>
2022-12-22 23:36:51 +0600

    todos passed to list component and showed by map

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/pages/InputField';
+import { Todo } from './boolHoooks';
+import TodoList from './components/Todolist/TodoList';
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo('');
+    }
+  }
+
+  console.log(todos)
   return (
     <div className="App">
       <span className='heading'>REXROX</span>
-      <InputField></InputField>
+      <InputField todo={todo} setTodo={setTodo}
+        handleAdd={handleAdd}
+      />
+      <TodoList todos={todos} setTodos={setTodos} />
+
     </div>
   );
 }
--- src/components/pages/InputField.tsx
+++ src/components/pages/InputField.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './input.css'
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+
+
   return (
     <div>
-      <form className='input'>
-        <input type="input" placeholder='Enter a task' className='input-box' />
+      <form
+        onSubmit={e => {
+          handleAdd(e)
+          inputRef.current?.blur();
+        }}
+        className='input'>
+        <input
+          ref={inputRef}
+          value={todo}
+          onChange={
+            e => setTodo(e.target.value)
+          }
+          type="input" placeholder='Enter a task' className='input-box' />
         <button className='input-submit' type='submit'>GO</button>
       </form>
     </div>
